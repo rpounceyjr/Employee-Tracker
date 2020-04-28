52bf48de5223273c3dbaf750d75f14e44dc0c406
rpounceyjr <rpounceyjr@gmail.com>
2020-04-27 22:18:50 -0400

    started inquirer functions.

--- employeeTracker.js
+++ employeeTracker.js
@@ -18,5 +18,39 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end()
+  start();
 });
+
+function start(){
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "start",
+            choices: ["Add an employee","Add a role", "Add a department"]
+        }
+    ]).then(function(answer){
+        if (answer.start === "Add an employee"){
+            console.log("add an employee");
+            connection.end();
+        }else if (answer.start === "Add a role"){
+            console.log("add a role");
+            connection.end();
+        }else{
+            console.log("Add a department");
+            connection.end();
+        }
+    })
+}
+// function addDepartment(){
+
+// }
+
+// function addEmployee(){
+    
+// }
+
+// function addRole(){
+    
+// }
