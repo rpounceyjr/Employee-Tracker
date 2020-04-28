6e477a1f3326b7d5687d556b0e3319d22500903a
rpounceyjr <rpounceyjr@gmail.com>
2020-04-27 22:05:03 -0400

    created files. created db and tables.

---
+++ tracker-schema.sql
@@ -0,0 +1,26 @@
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL(10,2),
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
