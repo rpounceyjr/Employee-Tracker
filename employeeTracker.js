17639a586efd9a4b81d0b4f42df4e937cccb67b4
rpounceyjr <rpounceyjr@gmail.com>
2020-04-28 13:36:46 -0400

    added viewDepartments function.

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,78 +1,120 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "employee_trackerDB"
+    // Your password
+    password: "",
+    database: "employee_trackerDB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  start();
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    start();
 });
 
-function start(){
+function start() {
     inquirer
-    .prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            name: "start",
-            choices: ["Add an employee","Add a role", "Add a department"]
-        }
-    ]).then(function(answer){
-        if (answer.start === "Add an employee"){
-            console.log("add an employee");
-            connection.end();
-        }else if (answer.start === "Add a role"){
-            console.log("add a role");
-            connection.end();
-        }else{
-            console.log("Add a department");
-            addDepartment();
-        }
-    })
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "start",
+                choices: ["Add an employee", "Add a role", "Add a department",
+                    "View employee", "View roles", "View departments"]
+            }
+        ]).then(function (answer) {
+            if (answer.start === "Add an employee") {
+                console.log("add an employee");
+                connection.end();
+            } else if (answer.start === "Add a role") {
+                console.log("add a role");
+                connection.end();
+            } else if (answer.start === "Add a department") {
+                console.log("Add a department");
+                addDepartment();
+            } else if (answer.start === "View employee") {
+                console.log("View employee");
+                connection.end();
+            } else if (answer.start === "View roles") {
+                console.log("View roles");
+                connection.end();
+            } else {
+                viewDepartment();
+            }
+        })
 }
-function addDepartment(){
+function addDepartment() {
     inquirer
-    .prompt([
-        {
-            name: "department",
-            type: "input",
-            message: "What department would you like to add?"
-         }
-    ])
-    .then(function(answer){
-        connection.query(
-        "INSERT INTO department SET ?",
-        {
-          name: answer.department
-        },
-        function(err) {
-          if (err) throw err;
-          console.log("Deparment added succesfully!");
+        .prompt([
+            {
+                name: "department",
+                type: "input",
+                message: "What department would you like to add?"
+            }
+        ])
+        .then(function (answer) {
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                    name: answer.department
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Department added succesfully!");
 
-          start();
-        }
-      );
-    })
-
-// }
-
-// function addEmployee(){
-    
-// }
-
-// function addRole(){
-    
+                    start();
+                }
+            );
+        })
 }
+
+function viewDepartment() {
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            console.log("\n" + res[i].name);
+        }
+    }
+    )
+start();
+}
+
+
+    // function addEmployee(){
+    //     inquirer
+    //     .prompt([
+    //         {
+    //             name: "name",
+    //             type: "input",
+    //             message: "What is this employee's name?"
+    //         }
+    //     ])
+    //     .then(function (answer) {
+    //         connection.query(
+    //             "INSERT INTO department SET ?",
+    //             {
+    //                 name: answer.department
+    //             },
+    //             function (err) {
+    //                 if (err) throw err;
+    //                 console.log("Deparment added succesfully!");
+
+    //                 start();
+    //             }
+    //         );
+    //     })
+
+    // }
+
+    // function addRole(){
+
--- tracker-schema.sql
+++ tracker-schema.sql
@@ -13,7 +13,8 @@ CREATE TABLE role (
   title VARCHAR(30),
   salary DECIMAL(10,2),
   department_id INT,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -21,6 +22,10 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT,
-  PRIMARY KEY (id)
+  manager_id INT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+--   FOREIGN KEY (manager_id) REFERENCES NULL
+--   figure out how to do self-referencing key
+
 );
